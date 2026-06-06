1c35a7554bad7e8ad206c49ddb41463657b3cec8
Drought-pulse <dsharma26nov@gmail.com>
2026-06-06 23:03:52 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,11 @@
-export default [
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
@@ -7,3 +14,5 @@ export default [
     },
   },
 ]
+
+export default eslintConfig
