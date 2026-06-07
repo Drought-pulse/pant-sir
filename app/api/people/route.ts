f5026c38f7d9ce3a0319238965f90aa428f32cf5
Drought-pulse <dsharma26nov@gmail.com>
2026-06-07 12:17:05 +0530

    Update route.ts

--- app/api/people/route.ts
+++ app/api/people/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import connectDB from "@/lib/dbConnect";
 import People from "@/models/People";
-
+export const dynamic = 'force-dynamic'
 await connectDB();
 
 // GET: Fetch person by ID
